199031421c99d4e42dd22ac182a5dbdd4c9d60c4
Jon <jpitts@flvc.org>
2014-06-12 15:27:52 -0400

    user model

---
+++ lib/user.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+require 'bcrypt'
+
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db.sqlite")
+
+class User
+  include DataMapper::Resource
+  include BCrypt
+
+  property :id, Serial
+  property :username, String, :length => 3..50, :key => true
+  property :password, BCryptHash
+  
+  def authenticate(attempted_password)
+    if self.password == attempted_password
+      true
+    else
+      false
+    end
+  end
+  
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
