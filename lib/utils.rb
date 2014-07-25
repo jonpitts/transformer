aed74d3f46331a9cf4bb7c7602e1f84d68c44bc6
Jon <jpitts@flvc.org>
2014-07-25 09:31:15 -0400

    Common utils module.

---
+++ lib/utils.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'fileutils'
+require 'zip'
+
+#Utilities module
+
+module Util
+  
+  module Zipper
+    
+    # Creates a zip
+    # Two parameters: zipfile_name, path to folder of collection to zip
+    def zip(zipfile_name, path)
+      puts zipfile_name
+      puts "what?"
+      Zip::File.open(zipfile_name, Zip::File::CREATE) do |zf|
+        Dir[File.join(path, "**/*")].each do |file|
+          puts file
+          zf.add(File.basename(file), file)
+        end
+      end
+    end
+    
+  end
+end
