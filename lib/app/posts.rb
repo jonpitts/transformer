526ca893c5d5fa4efef98b79a06a7c50f4b83845
Jon <jpitts@flvc.org>
2014-08-05 10:24:47 -0400

    Handle ajax and nonajax posts.

--- lib/app/posts.rb
+++ lib/app/posts.rb
@@ -33,29 +33,25 @@ post '/' do
     #transform xml into mods
     @@session[@user].transform tempfile, collection_id, institution
     "Process complete"
+  #handle non ajax requests
   else
-    "Ajax request not found.  Check your browser settings"
+    error 400, "Missing Data" unless params['xmlfile']
+    error 400, "Missing Data" if params['xmlfile'].empty?
+    
+    tempfile = params['xmlfile'][:tempfile]
+    filename = params['xmlfile'][:filename]
+    
+    client = env['REMOTE_ADDR']
+    
+    collection_id = params[:collection_id] unless params[:collection_id].empty?
+    institution = params[:institution] unless params[:institution].empty?
+    
+    #transform xml into mods
+    @@session[@user].transform tempfile, collection_id, institution
+    
+    redirect '/'
   end
 
-end
-
-#non ajax post method
-post '/nonajaxpost' do
-  error 400, "Missing Data" unless params['xmlfile']
-  error 400, "Missing Data" if params['xmlfile'].empty?
-    
-  tempfile = params['xmlfile'][:tempfile]
-  filename = params['xmlfile'][:filename]
-
-  client = env['REMOTE_ADDR']
-  
-  collection_id = params[:collection_id] unless params[:collection_id].empty?
-  institution = params[:institution] unless params[:institution].empty?
-  
-  #transform xml into mods
-  @@session[@user].transform tempfile, collection_id, institution
-
-  redirect '/'
 end
 
 post '/createHash' do
