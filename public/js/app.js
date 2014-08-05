75ca9a30a346726a97a6198d42dace6ba2d32e7d
Jon <jpitts@flvc.org>
2014-08-05 16:57:19 -0400

    More angular tests. Response data manipulation.

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,13 @@
 
   app.controller('MainController',['$scope', '$http', function($scope,$http){
     $http.get("/mods/")
-      .then(function(res){ $scope.tags = res.data;
+      .then(function(res){ 
+        var data = res.data; //response data is in json format
+        for (var key in data) {
+          var str = String(data[key]);
+          data[key] = str.replace(/[\[\"\ \]]/,''); //remove special characters from values
+        }
+        $scope.tags = data; //scope data into tags
       });
     
   }]);
