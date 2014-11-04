7b72d5802081a9c95306f66e13731102c2913d77
Jon <jpitts@flvc.org>
2014-11-04 16:05:46 -0500

    Util cruft removal.

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,9 +1,8 @@
 require 'rubygems'
 require 'fileutils'
 require 'zip'
-#require 'debugger'
-#Utilities module
 
+#Utilities module
 module Util
   
   module Zipper
@@ -27,12 +26,8 @@ module Util
     # call excelXML.jar to convert excel into xml
     #Two parameters: xlsxfile and outputfile
     def excelXML xlsxfile, outputfile, lock
-      puts outputfile
-      #debugger
-        #Dir.chdir(Dir.pwd + "/bin/") do
       lock.synchronize do
          `java -jar #{Dir.pwd}/bin/excelXML.jar #{xlsxfile} '#{outputfile}'`
-        #end
       end
       raise "ERROR :: Could not convert Excel document" unless File.exists?(outputfile)
     end
